e9ba0b1a7638604d862271d043149067bdd3d477
LYXOfficial <lyx2010official@outlook.com>
2023-05-06 17:32:41 +0800

    更新b1.1

--- NoTopDomain.build/__helpers.h
+++
@@ -1,8 +0,0 @@
-#ifndef __NUITKA_CALLS_H__
-#define __NUITKA_CALLS_H__
-
-extern PyObject *CALL_FUNCTION_WITH_ARGS11(PyObject *called, PyObject *const *args) ;
-extern PyObject *CALL_FUNCTION_WITH_ARGS12(PyObject *called, PyObject *const *args) ;
-extern PyObject *CALL_FUNCTION_WITH_ARGS13(PyObject *called, PyObject *const *args) ;
-extern PyObject *CALL_METHOD_WITH_ARGS12(PyObject *source, PyObject *attr_name, PyObject *const *args) ;
-#endif
